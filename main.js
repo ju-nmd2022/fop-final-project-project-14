ca9b11d0f7139147385aa00f74168ea53a4a0911
Jie C <117114870+jie5566@users.noreply.github.com>
2023-03-30 12:18:56 +0200

    skeleton-1
    start the skeleton structure building

---
+++ main.js
@@ -0,0 +1,19 @@
+let backgroundImage;
+let canvasPlayScreen;
+
+
+function preload() {
+  //   backgroundImage = loadImage("/images/background.png");
+}
+
+function setup() {
+  //   frameRate(30);
+  createCanvas(600, 750);
+  //   background(backgroundImage);
+  //   canvasPlayScreen = createCanvas(600, 400);
+  //   canvasPlayScreen.position(0, 300);
+}
+
+function draw() {
+  background(255);
+}
