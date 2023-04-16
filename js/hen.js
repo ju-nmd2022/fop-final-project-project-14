b4e90ebf859468d80687a33c4c482e409743246a
maso22ru <maso22ru@student.ju.se>
2023-04-16 18:04:30 +0200

    movement with arrowkeys Hen

--- js/hen.js
+++ js/hen.js
@@ -5,4 +5,16 @@ export class Hen extends Character {
         super(x, y, 0.05, ["/images/hen/hen0.png", "/images/hen/hen1.png", "/images/hen/hen2.png"]);
     }
 
+    move() {
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.x = this.x + 5;
+        } else if (keyIsDown(LEFT_ARROW)) {
+            this.x = this.x - 5;
+        } else if (keyIsDown(UP_ARROW)) {
+            this.y = this.y - 5;
+        } else if (keyIsDown(DOWN_ARROW)) {
+            this.y = this.y + 5;
+        }
+    }
+
 }
