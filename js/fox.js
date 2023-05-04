f9f6682a9301712aea1927153a2d04199543a0ac
Jie C <117114870+jie5566@users.noreply.github.com>
2023-05-04 17:33:04 +0200

    remove the old fox movement

--- js/fox.js
+++ js/fox.js
@@ -1,12 +1,17 @@
 import Character from "./character.js";
 
 export class Fox extends Character {
-    constructor(x, y) {
-      super(x, y, 0.05, ["/images/fox/fox0.png", "/images/fox/fox1.png", "/images/fox/fox2.png", "/images/fox/fox4.png", "/images/fox/fox5.png", "/images/fox/fox6.png", "/images/fox/fox7.png", "/images/fox/fox8.png", "/images/fox/fox9.png"]);
-    }
-
-    move () {
-      this.x = this.x + 5;
-      this.nextImage();
-    }
+  constructor(x, y) {
+    super(x, y, 0.05, [
+      "/images/fox/fox0.png",
+      "/images/fox/fox1.png",
+      "/images/fox/fox2.png",
+      "/images/fox/fox4.png",
+      "/images/fox/fox5.png",
+      "/images/fox/fox6.png",
+      "/images/fox/fox7.png",
+      "/images/fox/fox8.png",
+      "/images/fox/fox9.png",
+    ]);
+  }
 }
