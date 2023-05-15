262674d224a412f326d842934f961b7146f766fa
Jie C <117114870+jie5566@users.noreply.github.com>
2023-05-15 23:53:38 +0200

    relink

--- js/fox.js
+++ js/fox.js
@@ -3,15 +3,15 @@ const foxSpeed = 3;
 export class Fox extends Character {
   constructor(x, y) {
     super(x, y, 0.05, [
-      "../images/fox/fox0.png",
-      "../images/fox/fox1.png",
-      "../images/fox/fox2.png",
-      "../images/fox/fox4.png",
-      "../images/fox/fox5.png",
-      "../images/fox/fox6.png",
-      "../images/fox/fox7.png",
-      "../images/fox/fox8.png",
-      "../images/fox/fox9.png",
+      "/images/fox/fox0.png",
+      "/images/fox/fox1.png",
+      "/images/fox/fox2.png",
+      "/images/fox/fox4.png",
+      "/images/fox/fox5.png",
+      "/images/fox/fox6.png",
+      "/images/fox/fox7.png",
+      "/images/fox/fox8.png",
+      "/images/fox/fox9.png",
     ]);
 
     // Initialize the fox's movement direction, this slution is from chatGPT
